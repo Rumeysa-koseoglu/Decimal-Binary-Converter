842d6d42e3c4f0b2690f9ce45ab3d35a3de0e744
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-03-16 01:51:21 +0300

    feat: add binary to decimal conversion functionality

--- script.js
+++ script.js
@@ -8,3 +8,28 @@ decInp.addEventListener("input", () => {
     //Converts the decimal value to binary
     binInp.value = decValue.toString(2);
 });
+
+//Convert binary to decimal when user inputs in the binary field
+binInp.addEventListener("input", () => {
+    let binValue = binInp.value;
+    //If the binary number is valid, convert it to decimal
+    if (binValidator(binValue)) {
+    decInp.value = parseInt(binValue, 2);
+    errorMsg.textContent = "";
+    }
+    //Else display an error message
+    else {
+        errorMsg.textContent = "Please enter a valid binary input"
+    }
+
+    //Function to check if the binary number is valid
+    //  i.e it does not contain any number other than 0 and 1
+    function binValidator(num) {
+        for (let i = 0; i < num.length; i++) {
+            if (num[i] != "0" && num[i] != "1") {
+                return false;
+            }
+        }
+        return true;
+    }
+});
