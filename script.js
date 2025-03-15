122592915d93e347ea39e1bb614c6f0f088ce51c
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-03-16 01:29:17 +0300

    feat: add decimal to binary conversion functionality

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let decInp = document.getElementById("dec-inp");
 let binInp = document.getElementById("bin-inp");
 let errorMsg = document.getElementById("error-msg");
+
+//Convert decimal to binary when user inputs in the decimal field
+decInp.addEventListener("input", () => {
+    let decValue = parseInt(decInp.value);
+    //Converts the decimal value to binary
+    binInp.value = decValue.toString(2);
+});
